849e4d4a7687993a8329b1be4754239d04619e82
player64 <player.c64@gmail.com>
2020-04-20 17:55:12 +0100

    Support Gulp 4 added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,50 @@
 'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var inlineCss = require('gulp-inline-css');
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const inlineCss = require('gulp-inline-css')
+const browserSync = require('browser-sync').create()
+
+function scss() {
+    return gulp.src('./app/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./app/css'))
+}
+exports.scss = gulp.series(scss)
 
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task('scss:watch', (done) => {
+    gulp.watch('./app/sass/**/*.scss', gulp.series('scss'))
+    done()
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
 
+function copy_images() {
+    return gulp.src('./app/img/**/*')
+        .pipe(gulp.dest('./build/img'))
+}
 
-gulp.task('default', function() {
-    return gulp.src('./*.html')
+function inline_css() {
+    return gulp.src('./app/*.html')
         .pipe(inlineCss({
             applyStyleTags: true,
             applyLinkTags: true,
             removeStyleTags: false,
             removeLinkTags: true,
-            removeHtmlSelectors: true
+            removeHtmlSelectors: false
         }))
-        .pipe(gulp.dest('build/'));
-});
+        .pipe(gulp.dest('./build'))
+}
+exports.default = gulp.series(copy_images, scss, inline_css)
+
+gulp.task('serve', gulp.series(copy_images, scss, inline_css, (done) => {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    })
+    const watched_files = ['./app/img/**/*', './app/*.html','./app/sass/**/*.scss']
+
+    gulp.watch(watched_files, gulp.series('default'))
+    gulp.watch(watched_files).on('change', browserSync.reload)
+    done()
+}))
